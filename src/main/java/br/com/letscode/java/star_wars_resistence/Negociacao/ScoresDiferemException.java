6e7b5d77da2a6f97ee85bc10f3b1f1d944c5ed7d
Bruno Costa <brunoaocosta@hotmail.com>
2021-07-28 14:15:10 -0300

    Adaptação da classe reporte

---
+++ src/main/java/br/com/letscode/java/star_wars_resistence/Negociacao/ScoresDiferemException.java
@@ -0,0 +1,17 @@
+package br.com.letscode.java.star_wars_resistence.Negociacao;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ScoresDiferemException extends RuntimeException{
+    public ScoresDiferemException() {
+        super("As pontuações nessa negociação não estão adequadas! \n " +
+                "Confira a pontuação de cada item: \n" +
+                "Arma -> 4 pontos\n" +
+                "Munição -> 3 pontos\n" +
+                "Água -> 2 pontos\n" +
+                "Comida -> 1 ponto");
+    }
+
+}
---
+++ src/main/java/br/com/letscode/java/star_wars_resistence/Reporte/Reporte.java
@@ -0,0 +1,15 @@
+package br.com.letscode.java.star_wars_resistence.Reporte;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reporte {
+
+    private String idDenunciante;
+    private String idTraidor;
+
+}
